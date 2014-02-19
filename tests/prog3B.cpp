46c25d956206dca46bd997766231d5f74fbed51b
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2014-02-18 18:03:48 -0800

    This almost works\!

--- tests/prog3B.cpp
+++ tests/prog3B.cpp
@@ -1,8 +1,26 @@
-#include "../src/Main.h"
+#include <Main.h>
+#include <err.h>
+
+#include <iostream>
+#include <string>
+#include <limits>
+using namespace std;
 
 int main()
 {
-	Main::run();
+	std::string s;
+	try {
+		Main::run();
+	} catch( UncatchableError& e ) {
+		std::cout << 
+			"According to Dr. C, the only errors we may check for are "
+			"those which he specifically noted. Therefore, I have not "
+			"implemented error checking for this specific scenario. "
+			"Anything could happen. I wonder what will?"
+			"\nPress ENTER to continue.\n";
+		cin.ignore( std::numeric_limits< streamsize >::max(), '\n' );
+		getline( cin, s );
+	}
 	
 	return 0;
 }
